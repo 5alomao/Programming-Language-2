1ff9868ccdd5fda09d171c9968370de0666c6156
Salomão <101257752+5alomao@users.noreply.github.com>
2023-04-18 13:44:25 -0300

    Alterando para filha direta de Exception

--- BancoSI/src/exceptions/ValorInvalidoException.java
+++ BancoSI/src/exceptions/ValorInvalidoException.java
@@ -1,6 +1,6 @@
 package exceptions;
 
-public class ValorInvalidoExceptions extends RuntimeException {
+public class ValorInvalidoExceptions extends Exception {
 
   /*
   Atenção: nem sempre é interessante criarmos um novo tipo de exception! Depende do caso. Neste aqui, seria
