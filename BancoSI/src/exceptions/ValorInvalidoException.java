5a795f516f64cd54af4c91a6ffa4e8362899f39b
Salomão <101257752+5alomao@users.noreply.github.com>
2023-04-18 13:35:40 -0300

    Create ValorInvalidoException.java

---
+++ BancoSI/src/exceptions/ValorInvalidoException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class ValorInvalidoExceptions extends RuntimeException {
+
+  /*
+  Atenção: nem sempre é interessante criarmos um novo tipo de exception! Depende do caso. Neste aqui, seria
+  melhor ainda utilizarmos IllegalArgumentException. A boa prática diz que devemos preferir usar as já existentes do
+  Java sempre que possível.
+  */  
+
+}
