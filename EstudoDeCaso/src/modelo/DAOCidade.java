625639b2de9f355e3d9604291d62f1f28c8500fe
Salomao Ferreira Junior <17413319608@campus.machado.mch>
2023-04-24 08:33:04 -0300

    Estudo De Caso Adicionado

---
+++ EstudoDeCaso/src/modelo/DAOCidade.java
@@ -0,0 +1,23 @@
+package modelo;
+
+import java.util.List;
+
+public class DAOCidade { //Data Acess Object (DAO)
+    public List<Cidade> getLista(){
+        return Dados.listaCidade;
+    }
+    
+    public boolean salvar(Cidade obj){
+        if(obj.getCodigoCidade()==null){
+            Integer codigo = Dados.listaCidade.size() + 1;
+            obj.setCodigoCidade(codigo);
+            Dados.listaCidade.add(obj);
+        }
+        return true;
+    }
+    
+    public boolean remover(Cidade obj){
+        Dados.listaCidade.remove(obj);
+        return true;
+    }
+}
---
+++ EstudoDeCaso/src/modelo/DAOFuncionario.java
@@ -0,0 +1,23 @@
+package modelo;
+
+import java.util.List;
+
+public class DAOFuncionario { //Data Acess Object (DAO)
+    public List<Funcionario> getLista(){
+        return Dados.listaFuncionario;
+    }
+    
+    public boolean salvar(Funcionario obj){
+        if(obj.getCodigoFuncionario()==null){
+            Integer codigo = Dados.listaFuncionario.size() + 1;
+            obj.setCodigoFuncionario(codigo);
+            Dados.listaFuncionario.add(obj);
+        }
+        return true;
+    }
+    
+    public boolean remover(Funcionario obj){
+        Dados.listaFuncionario.remove(obj);
+        return true;
+    }
+}
