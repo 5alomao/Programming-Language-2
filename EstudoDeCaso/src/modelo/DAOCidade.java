970aa5932b6ae6d0ca81e9fb12211173325e6811
Salomao Ferreira Junior <17413319608@campus.machado.mch>
2023-06-12 08:00:49 -0300

    Método Alterar - DAOCidade

--- EstudoDeCaso/src/modelo/DAOCidade.java
+++ EstudoDeCaso/src/modelo/DAOCidade.java
@@ -43,6 +43,35 @@ public class DAOCidade { //Data Acess Object (DAO)
             JOptionPane.showMessageDialog(null, "Erro de SQL no incluir do DAOCidade" + e.getMessage());
         }
         return false;
-  }
+    }
+    
+    public boolean alterar(Cidade obj) {
+        String sql = "update cidade set nome = ?, uf = ? where codCidade = ?"; // ? = valores do formulário
+        try {
+            PreparedStatement pst = Conexao.getPreparedStatement(sql); // passando comando sql pra conexão
+            pst.setString(1, obj.getNomeCidade()); // 1 = primeira ? = nome
+            pst.setString(2, obj.getUfCidade()); // 2 = segunda ? = uf
+            pst.setInt(3, obj.getCodigoCidade()); // 3 = terceiro ? = codCidade
+            if (pst.executeUpdate() > 0) {
+                JOptionPane.showMessageDialog(null, "Cidade alterada");
+                return true;
+            } else {
+                JOptionPane.showMessageDialog(null, "Cidade não alterada");
+                return false;
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro de SQL no alterar do DAOCidade" + e.getMessage());
+        }
+        return false;
+    }
+    
+    public boolean salvar(Cidade obj) {
+        if (obj.getCodigoCidade() == null) {
+            return incluir(obj);
+        } else {
+            //return alterar(obj);
+            return true;
+        }
+    }
     
 }
